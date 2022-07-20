be2b17978c51f32b97faae90a61e24a3dc5962ba
KIMHUEMANG <s22043@gsm.hs.kr>
2022-07-20 12:36:18 +0900

    main , start

--- js/main.js
+++ js/main.js
@@ -1,14 +1,50 @@
-const $hole = document.querySelector('.post1 .black-hole');
-const $letsBlack = document.querySelector(".post1 .lets_hole");
+// sun
+const $sun = document.querySelector(".intro_sun");
+const $sunIntro = document.querySelector(".intro_sun .sun");
+const $sunExplain = document.querySelector(".intro_sun .sun_explain");
 
-function handleBlackHole() {
-  
-  $letsBlack.innerHTML = `클릭해서 우주로 가기`
-}
+// galaxy
+const $galaxy = document.querySelector(".intro_galaxy");
+const $galaxyIntro = document.querySelector(".intro_galaxy .galaxy");
+const $galaxyExplain = document.querySelector(".intro_galaxy .galaxy_explain");
 
-function blackHoleOut(){
-  $letsBlack.innerHTML = ``;
-}
+const FADEIN = ' fade-in';
 
-$hole.addEventListener("mouseover",handleBlackHole);
-$hole.addEventListener("mouseout", blackHoleOut);
+let sunFadeCount = 0;
+let galaxyFadeCount = 0;
+
+// 태양계 소개 fade-in
+$sun.addEventListener("mouseover",function(){
+  if(sunFadeCount === 0){
+  $sunIntro.className += FADEIN;
+  $sunExplain.className += FADEIN;
+  sunFadeCount++;
+  }
+});
+
+$sun.addEventListener("mouseout", function(){
+  if(sunFadeCount == 1){
+    $sunIntro.className = 'sun';
+    $sunExplain.className = 'sun_explain';
+    sunFadeCount--;  
+  }
+});
+
+//galaxy Fade-in
+
+$galaxy.addEventListener("mouseover",function(){
+  if(galaxyFadeCount === 0){
+  $galaxyIntro.className += FADEIN;
+  $galaxyExplain.className += FADEIN;
+  galaxyFadeCount++;
+  }
+});
+
+$galaxy.addEventListener("mouseout", function(){
+  if(galaxyFadeCount == 1){
+    $galaxyIntro.className = 'galaxy';
+    $galaxyExplain.className = 'galaxy_explain';
+    galaxyFadeCount--; 
+  }
+});
+
